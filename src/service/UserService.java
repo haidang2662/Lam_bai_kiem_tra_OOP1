825c98959262108908c86f09f1676643e60adaf3
haidang2662 <141023574+haidang2662@users.noreply.github.com>
2024-03-04 22:27:25 +0700

    push them tong 3 phuong thuv trong menu

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -90,6 +90,9 @@ public class UserService {
         }
         System.out.println("Moi ban nhap password : ");
         userLogin.setPassWord(scanner.nextLine());
+
+//        users.add(userLogin);
+//        System.out.println(users);
         notFounded = true;
         Menu menu = new Menu();
         while (notFounded) {
@@ -97,95 +100,15 @@ public class UserService {
             for (User user : users) {
                 if (!user.getPassWord().equalsIgnoreCase(userLogin.getPassWord())) {
                     System.out.println("Nhap password sai kiểm tra lại Password : ");
-                    menu.dilayMenuLogin(scanner,users);
+                    menu.dilayMenuLogin(scanner,users,userLogin);
                     notFounded=true;
                     break;
+                } else {System.out.println("Chuc mung ban nhap dung password");
+                    menu.displayMenuLast(scanner,users,userLogin);
                 }
             }
             break;
         }
-        System.out.println("chao mung "+(userLogin.getUserName()+", ban co the thuc hien cac cong viec sau"));
-        System.out.println("1 : Thay doi username : ");
-        System.out.println("2 : Thay doi email : ");
-        System.out.println("3 : Thay doi mat khau  : ");
-        System.out.println("4 : Dang xuat quay ve muc dang ky : ");
-        System.out.println("0 : Thoat khoi chuong trinh  ");
-        System.out.println("Please chooose : ");
-        int choice2 = Integer.parseInt(scanner.nextLine());
-        UserService userService = new UserService();
-        switch (choice2){
-
-            case 1 :
-                System.out.println("Da chon 1 : ");
-                System.out.println("Moi ban nhap username moi : ");
-                User newUser = new User();
-                newUser.setUserName(scanner.nextLine());
-                notFounded = true;
-                while (notFounded) {
-                    notFounded=false;
-                    for (User user : users) {
-                        if (user.getUserName().equalsIgnoreCase(newUser.getUserName())) {
-                            System.out.println("Ten dang nhap da ton tai vui long chon ten khac : ");
-                            newUser.setUserName(scanner.nextLine());
-                            notFounded=true;
-                            break;
-                        }
-                    }
-                }
-                System.out.println("chuc mung ban da thay doi username thanh cong ");
-                userLogin.setUserName(newUser.getUserName());
-                System.out.println("Username moi cua ban la "+(userLogin.getUserName()));
-                break;
-            case 2 :
-                System.out.println("Da chon 2 : ");
-                System.out.println("Moi ban nhap email moi : ");
-                User newUser1 = new User();
-                newUser1.setEmail(scanner.nextLine());
-                notFounded = true;
-                while (notFounded) {
-                    notFounded=false;
-                    for (User user : users) {
-                        if (user.getEmail().equalsIgnoreCase(newUser1.getEmail())) {
-                            System.out.println("Email da ton tai vui long chon email khac : ");
-                            newUser1.setEmail(scanner.nextLine());
-                            notFounded=true;
-                            break;
-                        }
-                    }
-                }
-                System.out.println("chuc mung ban da thay doi email thanh cong ");
-                userLogin.setEmail(newUser1.getEmail());
-                System.out.println("Email moi cua ban la "+(userLogin.getEmail()));
-                break;
-            case 3 :
-                System.out.println("Da chon 3 : ");
-                System.out.println("Moi ban nhap mat khau moi : ");
-                User newUser2 = new User();
-                newUser2.setPassWord(scanner.nextLine());
-                boolean notAccept=true;
-                while (notAccept) {
-                    notAccept=false;
-                    if (!newUser2.getPassWord().matches("^(?=.*[A-Z])(?=.*[.,-_;])[a-zA-Z0-9.,-_;]{7,15}$")) {
-                        System.out.println("Password khong hop le chon password khac : ");
-                        newUser2.setPassWord(scanner.nextLine());
-                        notAccept=true;
-                    }
-                }
-                System.out.println("Chuc mung doi password thanh cong ");
-                userLogin.setPassWord(newUser2.getPassWord());
-                System.out.println("Password moi la "+userLogin.getPassWord());
-
-                break;
-            case 4 :
-                System.out.println("Da chon 4 : ");
-                menu.dilayMenuLogin(scanner,users);
-                break;
-            case 0 :
-
-                break;
-
-        }
-
 
         return userLogin;
     }
